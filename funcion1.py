94c87a6212df6cc7f7e4797af979066e137c13c5
Edicilio Lopez <lopez.edicilio@gmail.com>
2023-05-22 10:37:46 -0300

    parcial finalizado

--- funcion1.py
+++ funcion1.py
@@ -1,1 +1,16 @@
-print("Funcion1")
+## Crear una función llamada aplicarAumento que reciba como parámetro el precio de un producto y 
+##devuelva el valor del producto con un aumento del 5%.
+##realizar la llamada desde el main.
+
+##def aplicarAumento(precio:float): -> float
+##La funcion AplicarAumento recibe como parametro el precio de un producto y realiza un aumento del 5%
+##retornando un valor tipo float
+def aplicarAumento(precio):
+    aumento = precio * 0.05
+    precioConAumento = precio + aumento
+    return precioConAumento
+
+
+precioProducto = float(input("Ingrese el precio del producto: "))
+precioConAumento = aplicarAumento(precioProducto)
+print("El precio con aumento es:", precioConAumento)
--- funcion2.py
+++ funcion2.py
@@ -1,1 +1,28 @@
-print("Funcion2")
+# 2. Crear una función que se llame reemplazarCaracteres que reciba una cadena de caracteres
+# como primer parámetro, un carácter como segundo y otro carácter  como tercero,  
+# la misma deberá reemplazar cada ocurrencia del segundo parámetro por el tercero 
+# y devolver la cantidad de veces que se reemplazo ese carácter  en la cadena
+
+##def reemplazarCaracteres(cadena, caracter_original, caracter_reemplazo): la funcion recibe 3 parametros tipo string
+## el primero es una cadena de caracteres, el segundo un caracter y el tercero un caracter y la misma 
+## retorna 2 valores con la nueva cadena y la cantidad de veces que se reemplazó ese caracter 
+
+def reemplazarCaracteres(cadena, caracter_original, caracter_reemplazo):
+    cantidad_reemplazos = 0
+    nueva_cadena = ""
+    for caracter in cadena:
+        if caracter == caracter_original:
+            nueva_cadena += caracter_reemplazo
+            cantidad_reemplazos += 1
+        else:
+            nueva_cadena += caracter
+    return nueva_cadena, cantidad_reemplazos
+
+
+cadena = input("Ingrese una cadena de caracteres: ")
+caracter_original = input("Ingrese el carácter a reemplazar: ")
+caracter_reemplazo = input("Ingrese el carácter de reemplazo: ")
+
+nueva_cadena, cantidad_reemplazos = reemplazarCaracteres(cadena, caracter_original, caracter_reemplazo)
+print("Nueva cadena:", nueva_cadena)
+print("Cantidad de reemplazos realizados:", cantidad_reemplazos)
